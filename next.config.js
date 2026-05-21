c371ab818801aa9b62c9ed7f9798cc843f53921d
SITT-TIJUANA <gjvazquez@tijuana.gob.mx>
2026-05-21 01:33:56 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
 
 module.exports = nextConfig
